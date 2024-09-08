bbc1baa8d8b3c366676570c4b9ba8df930874e07
Sérgio <sergio.filho@pdcase.com.br>
2024-09-08 12:46:39 -0300

    fix: adição de notificações toast para notificação.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,7 +5,8 @@ import React, { useEffect, useState, useRef} from 'react'
 import ReactDOM from 'react-dom';
 import { Link, useNavigate} from 'react-router-dom';
 import { BrowserRouter, Route, Routes} from "react-router-dom"
-
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 //JS
 function Home() {
@@ -17,31 +18,62 @@ function Home() {
 
   const [users, setUsers] = useState([]);
 
+  const [message, setMessage] = useState('');
+
   async function getUsers(){
     const usersAPi = await api.get('/pacientes')
     setUsers(usersAPi.data)
   }
-
+  //método para atualizar os pacientes após cadastrar ou apagar, ou quando entrar na pag
   useEffect(() =>{
     getUsers()
   }, [])
 
   //método POST
   async function createUsers() {
-    await api.post('/pacientes',{
-      nome: inputNome.current.value,
-      carteira: inputCarteira.current.value,
-      plano: inputPlano.current.value,
-      especialidade: inputEspecialidade.current.value
-    });
-    
-    //quando insere o paciente novo, precisa atualizar a pagina, o getusers faz isso
-    getUsers()
+    try {
+      
+      const response = await api.post('/pacientes', {
+        nome: inputNome.current.value,
+        carteira: inputCarteira.current.value,
+        plano: inputPlano.current.value,
+        especialidade: inputEspecialidade.current.value
+      });
+      toast.success(response.data.message);
+  
+      // Atualiza a lista de pacientes
+      getUsers();
+  
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+
+        toast.error(`Erro: ${error.response.data.message}`, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      } else {
+        toast.error('Erro ao cadastrar o paciente. Tente novamente.', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
   }
+}
 
   //método DELETE
   async function deleteUsers(id){
     await api.delete(`/pacientes/${id}`)
+    toast.success('Paciente deletado com sucesso!')
     getUsers()
   }
 
@@ -51,6 +83,7 @@ function Home() {
 
   //HTML
   return (
+    
     <div className='container'>
       <form action="">
         <h1>Cadastro de Pacientes</h1>
@@ -60,6 +93,8 @@ function Home() {
         <input placeholder='Especialidade' name='especialidade' type="text"ref={inputEspecialidade} />
         <button type='button' onClick={createUsers}>Cadastrar</button>
       </form>
+
+      <ToastContainer></ToastContainer>
 
       <p className='titulocard'>Pacientes cadastrados</p>
 
@@ -75,16 +110,13 @@ function Home() {
             <p>Plano: <span>{user.plano}</span></p>
             <p>Especialidade: <span>{user.especialidade}</span></p>
           </div>
-            
+          
           <button onClick={() => deleteUsers(user.id)}>
             <img className='thrash' src={Thrash} alt="thrash" />
           </button>
         </div>
-
       )))}
-    </div>
-
-
+    </div>    
   )
 }
 
